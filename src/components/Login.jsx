3e1601a0f6447c69dfacebae1f7d69053c45f770
Rohama Majeed <rohamamajeed3@gmail.com>
2026-06-09 17:05:05 +0300

    files

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -265,14 +265,14 @@ const Login = () => {
             )}
 
             {/* Social Login Section */}
-            <div className="relative">
+            {/* <div className="relative">
               <div className="absolute inset-0 flex items-center">
                 <div className="w-full border-t border-gray-300"></div>
               </div>
               <div className="relative flex justify-center text-sm">
                 <span className="px-2 bg-white text-gray-500">Or continue with</span>
               </div>
-            </div>
+            </div> */}
 
             <div className="grid grid-cols-2 gap-3">
               <button
